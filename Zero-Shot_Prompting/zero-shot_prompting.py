f153a34de963cf164f92cdb2251fba4c96a5c5da
ayuk007 <ayush.nashine4807@gmail.com>
2024-12-22 18:19:53 +0530

    Zero-Shot prompting updated

--- Zero-Shot_Prompting/zero-shot_prompting.py
+++ Zero-Shot_Prompting/zero-shot_prompting.py
@@ -5,16 +5,36 @@ load_dotenv(find_dotenv())
 
 groq_client = Groq()
 
-prompt = "Translate this sentence into Spanish: 'I love programming.'"
+system_prompt = "You are a good translator. Whenever user asks a question in English, translate it to French."
 
-response = groq_client.chat.completions.create(
+groq_client.chat.completions.create(
     model = "llama3-8b-8192",
     messages = [
         {
-            "role": "user",
-            "content": prompt
-        }
+            "role": "system",
+            "content": system_prompt
+        },
     ]
 )
 
-response.choices[0].message.content
+def chat_completion(user_prompt):
+
+    response = groq_client.chat.completions.create(
+                    model = "llama3-8b-8192",
+                    messages = [
+                        {
+                            "role": "system",
+                            "content": system_prompt
+                        },
+                        {
+                            "role": "user",
+                            "content": user_prompt
+                        }
+                    ]
+                )
+    return response.choices[0].message.content
+ 
+if __name__ == "__main__":
+
+    user_prompt = input("Ask a question: ")
+    print("Answer: ", chat_completion(user_prompt))    
